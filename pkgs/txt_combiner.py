f52ad844dad33e730d8e036c47ce46f0f74d93f7
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-24 20:09:43 +0100

    Add: txt combiner gets all the .txt and combine them together in just one .txt

---
+++ pkgs/txt_combiner.py
@@ -0,0 +1,30 @@
+"""The txt_combiner reads in all the .txt files and combine them in one single file."""
+
+import os
+import sys
+import glob
+from pathlib import Path
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pkgs.text_contour_finding import path_normalizer
+
+
+txt_dir = "sandbox/text_extraction"
+# grab each file in each directory: when recursive is set, ** followed by a path separator matches 0 or more subdirectories.
+txt_folder = glob.glob(f"{txt_dir}/*", recursive=False)
+# print(txt_folder)
+
+for each_folder in txt_folder:
+    each_folder = path_normalizer(each_folder)
+    # grab all the filename of the specific folder where the txts are stored
+    filenames = glob.glob(f"{each_folder}/*", recursive=True)
+
+    # grab only the folder name to be fed as a filename for the final entire txt file
+    folder_path_to_name = Path(each_folder)
+    folder_name = folder_path_to_name.name
+
+    with open(f"{txt_dir}/result_{folder_name}.txt", "wb") as outfile:
+        for f in filenames:
+            with open(f, "rb") as infile:
+                # about the \n encoding in ascii: https://stackoverflow.com/questions/21916888/cant-concat-bytes-to-str-when-using-subprocess-check-output
+                outfile.write(infile.read() + "\n".encode("ascii"))
