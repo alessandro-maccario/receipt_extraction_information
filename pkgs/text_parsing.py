85cde94a379122b742e7d62fab2b86fedf09a985
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-11-25 06:57:51 +0100

    Refactor: refactor variable names from test to more appropriate names. Fix logic of text extraction, parsing and processing

--- pkgs/text_parsing.py
+++ pkgs/text_parsing.py
@@ -1,56 +1,60 @@
+import os
+import sys
 import re
+import glob
 import pandas as pd
 
+# Add the root folder to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from pkgs.text_contour_finding import (
+    path_normalizer,
+)
 
-def regex_substitution(text):
-    """
-    Remove the spaces between digits in a string and substitute the "," with a "."
-    """
-    remove_spaces_between_numbers = re.compile(r"(\d+)\s*,\s*(\d+)")
-    text_substitution = re.sub(remove_spaces_between_numbers, r" \1.\2 ", text)
+# path to the folder containing the text extracted from the images
+text_extraction_dir = "sandbox/text_extraction"
 
-    return text_substitution
-
-
-example = "U,UU,Glas Wasser / Ieer,X Glas Masser /Ieer,0,00,Salat groß,2 , 40 Datum :,12/11/2024"
-example_2 = """
-Jennite
-U,UU,Glas Wasser / Ieer,X Glas Masser /Ieer,0,00,Salat groß,2 , 40
-Datum :,12/11/2024,Vihkainfineon77,KundenNr ,30177616,Bon-Nr ,,880338
-Siemensstrale,9500,Vilach
-Inf ineon Technofogves
-Dussmann,Austria,GmbH
-RKSV-Resse-Hr : %iKO,Kasse,VIHKAINFINEON
-Herzlichen Dank Tür  Ihren Besuch
-ATu 23260209
-204,0,09 Netto 208,0,00 llust. 203
-40 Metto,2, 10 Hust.705
-omnabbuchuna,4u
-Sumne,2 ,40
-
-"""
-
+# regex patterns
 remove_spaces_between_numbers = re.compile(r"(\d+)\s*,\s*(\d+)")
-
-text_substitution = re.sub(remove_spaces_between_numbers, r" \1.\2 ", example_2)
-print("Substituted text:", text_substitution)
-
-
-# date matcher
 date_matcher = r"(\d+\/\d+\/\d+)"
-date_finder = re.findall(date_matcher, text_substitution)
-print("Date:", date_finder)
-
-
-# remove the forward slashes in the text (the date should have already been parsed!)
-text_substitution = text_substitution.replace("/", "")
-
-# based on the following regex, create a list of tuples that contains (item, price)
 # (?:...) is a non-capturing group, meaning it groups things together without extracting them as separate matches.
 # The ? at the end makes the entire group optional. This means that a number like 2 (no decimal part) will still match, but if there is a decimal, it must follow the [0-9]+ rules.
 regx_pattern_item_price = r"([a-zA-ZäöüßÄÖÜéÉ ]+),\s*([0-9]+(?:[.,][0-9]+)?)"
-test_item_price = re.findall(regx_pattern_item_price, text_substitution)
-print("Test item, price:", test_item_price)
+
+# find the txt to be processed
+text_extracted = glob.glob(f"{text_extraction_dir}/*.txt", recursive=False)
+print("TEXT EXTRACTED:", text_extracted)
 
 
-print("DF:\n", pd.DataFrame(list(test_item_price), columns=["item", "price"]))
+for txt_file in text_extracted:
+    txt_file = path_normalizer(txt_file)
+
+    with open(txt_file) as txt_processing:
+        # print the entire text
+        # print("".join(txt_processing))
+        txt_content = "".join(txt_processing)
+        # print(txt_processing.readlines())  # list containing lines of file)
+
+        text_substitution = re.sub(
+            remove_spaces_between_numbers, r" \1.\2 ", txt_content
+        )
+        # print("Substituted text:", text_substitution)
+
+        # date matcher
+        date_finder = re.findall(date_matcher, text_substitution)
+        # print("Date:", date_finder)
+
+        # remove the forward slashes in the text (the date should have already been parsed!)
+        text_substitution = text_substitution.replace("/", "")
+
+        # based on the following regex, create a list of tuples that contains (item, price)
+        item_price = re.findall(regx_pattern_item_price, text_substitution)
+        # remove the " " element in the list
+        item_price = [item_tuple for item_tuple in item_price if item_tuple[0] != " "]
+        # remove elements if the price is not #.## but integer or a big number (30177616 for instance)
+        item_price = [item_tuple for item_tuple in item_price if "." in item_tuple[1]]
+        print("Test item, price:", item_price)
+
+        # # convert and save the pd.DataFrame into a csv
+        # df = pd.DataFrame(list(test_item_price), columns=["item", "price"])
+        # print(df)
+        # df.to_csv("sandbox/text_extraction/result_original.csv", index=False)
