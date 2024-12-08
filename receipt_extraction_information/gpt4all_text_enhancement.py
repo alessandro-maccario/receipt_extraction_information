107751d745b332717e0c690eff4aedf38dfaca1f
alessandro-maccario <a.maccario1@campus.unimib.it>
2024-12-08 16:56:36 +0100

    Refactoring: remove old comments

--- receipt_extraction_information/gpt4all_text_enhancement.py
+++ receipt_extraction_information/gpt4all_text_enhancement.py
@@ -2,13 +2,15 @@
 output to be correctly formatted before using it in the main .csv file.
 """
 
+#######################
+### IMPORT PACKAGES ###
+#######################
+
 import glob
 from gpt4all import GPT4All
 import os
 import pandas as pd
 from pathlib import Path
-import pprint
-import time
 from tqdm import tqdm
 import sys
 
@@ -16,6 +18,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from pkgs.utils import path_normalizer, md2csv
 from pkgs.constants import LLAMA_32_3B_MODEL
 
+#######################
+######## MAIN #########
+#######################
 
 # path to the folder containing the text extracted from the images
 text_extraction_dir = "sandbox/text_extraction"
@@ -24,7 +29,6 @@ text_extracted = glob.glob(f"{text_extraction_dir}/*_result.csv", recursive=Fals
 
 # chose the model to be used for polishing the data into a .csv format
 MODEL_PATH = LLAMA_32_3B_MODEL
-# MODEL_PATH = os.path.abspath("model/Llama-3.2-3B-Instruct-Q4_0")
 
 # call the model just once, not at every iteration
 model = GPT4All(
@@ -37,10 +41,9 @@ for csv_file in tqdm(text_extracted):
     csv_file_path_to_name = Path(csv_file)
     csv_filename = csv_file_path_to_name.name[:-4]
 
+    # normalize the backslash to forward slash
     csv_file = path_normalizer(csv_file)
-    # print("\nCSV FILE:", csv_file, "\n")
 
-    # TODO: TEST WITH ONE CSV: READ THE CSV CREATED BY THE text_parsing.py SCRIPT AND CONVERT IT TO MD BEFORE FEEDING IT INTO GPT4ALL
     # Load the data
     df = pd.read_csv(csv_file)
 
@@ -64,20 +67,15 @@ for csv_file in tqdm(text_extracted):
                         Remove any rows that is not related to an shopping item.
                         """
 
-    # # start counting the time needed for the model to be loaded
-    # start_time = time.time()
-
+    # feed the model with the prompt and generate response
     with model.chat_session():
         response = model.generate(PROMPT_RECEIPT_V3)
-        # print(response)
-        # pprint.pprint(model.current_chat_session)
         content = model.current_chat_session
 
     # extract the content if the role is assistant that matches the answer from the model
     for json_dict in content:
         if json_dict["role"] == "assistant":
             md_item_price_table = "".join(json_dict["content"])
-            # print(json_dict["content"])
 
     # convert the markdown output into a csv table
     markdown_to_csv_conversion = md2csv(md_item_price_table)
@@ -87,5 +85,3 @@ for csv_file in tqdm(text_extracted):
     markdown_to_csv_conversion.to_csv(
         f"sandbox/text_extraction/{csv_filename}_cleaned.csv", index=False
     )
-
-    # print("--- %s seconds ---" % (time.time() - start_time), "\n")
